7ca21eb478408b0ea3163e58eb27b5c88fdf34a4
yumejensen <yumejensen@gmail.com>
2025-03-22 16:15:28 -0500

    range

--- day-3/homework/range.js
+++ day-3/homework/range.js
@@ -20,15 +20,34 @@
  *  a. create something to collect the output you'll return.
  *  b. you'll need a loop, which one is best?
  *  c. you'll wanna make use of the push() method of Array.
+ 
+ I: Two numbers as parameters
+ O: An array containing all the integers between the two numbers
+ C: use a loop and the push method
+ E: if the 1st number is greater than the 2nd, return the range in reverse order
  */
-function range(start, end) {
+ function range(start, end) {
     // YOUR CODE GOES BELOW HERE //
-    
-    
-    
-    
+  // holder array for my numbers range
+  var numbersRange = [];
+  // if start < end - for loop forwards and push
+  if (start < end){
+    for (var i = start; i <= end; i++){
+      numbersRange.push(i);
+  // if start > end - for loop backwards other way and push
+    }
+  } else if (start > end){
+    for (var j = start; j >= end; j--){
+      numbersRange.push(j);
+    }
+  }
+  // return numbersRange array
+  return numbersRange;
     // YOUR CODE GOES ABOVE HERE //
 }
+
+//console.log(range(0, 5)); // works!
+//console.log(range(6, 1)); // works!
 
 
 
